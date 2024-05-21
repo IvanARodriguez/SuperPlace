f217e3ca7430fdc6c0a2dafc170925da6fd4652f
Ivan A Rodriguez <ivancalvo91@gmail.com>
2024-05-21 15:23:50 -0400

    setp view engine

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
 import express from 'express'
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 const app = express()
 
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'))
+
+app.get('/', (req, res)=>{
+  res.render('home')
+})
+
 app.listen(3000, ()=>{
-  console.log('[INFO] - Application listening on port 3000')
+  console.log('[INFO] - 🖥 server listening on port 3000')
 })
